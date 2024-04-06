549b47dfc8d524d0afa52caa298af687c1b3cc92
Mikael Andersson <qubex2@gmail.com>
2024-04-06 03:20:01 +0200

    w

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "Sans", "sans-serif"],
+    },
     extend: {
       colors: {
         ...colors,
